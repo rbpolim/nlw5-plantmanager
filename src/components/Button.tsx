643fbcdc83cc8b5e65d2862fa99416f590e404db
rbpolim <rodrigobpolim@gmail.com>
2021-04-20 19:40:27 -0300

    Aula-2

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps
+} from 'react-native';
 
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
-export function Button() {
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export function Button({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.button} activeOpacity={0.7}>
-      <Text style={styles.buttonText}>
-        Avançar
+    <TouchableOpacity
+      style={styles.container}
+      {...rest}
+    >
+      <Text style={styles.text}>
+        { title }
       </Text>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
+    height: 56,
+    borderRadius: 16,
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
-    height: 56,
-    width: 56,
   },
-  buttonText: {
+  text: {
+    fontSize: 16,
     color: colors.white,
-    fontSize: 24,
-  }
+    fontFamily: fonts.heading,
+  },
 });
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,29 +1,55 @@
 import React from 'react';
-
-import { SafeAreaView, Text, View, Image, TouchableOpacity, StyleSheet, ColorPropType } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  View,
+} from 'react-native';
 
 import wateringImg from '../assets/watering.png';
-import { Button } from '../components/Button';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  function handleStart() {
+    navigation.navigate('UserIndentification')
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
-      </Text>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de{'\n'}
+          forma fácil
+        </Text>
 
-      <Image source={wateringImg} style={styles.image} />
+        <Image
+          source={wateringImg}
+          style={styles.image}
+          resizeMode="contain"
+        />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas.
-        Nós cuidamos de lembrar para você sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar para você sempre que precisar.
+        </Text>
 
-      <Button />
-
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather name="chevron-right" style={styles.buttonIcon}/>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -31,25 +57,30 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontFamily: fonts.heading,
     color: colors.heading,
+    fontSize: 28,
+    lineHeight: 34,
+    textAlign: 'center',
     marginTop: 38,
   },
   image: {
-    width: 292,
-    height: 284,
+    height: Dimensions.get('window').width * 0.7,
   },
   subtitle: {
+    fontFamily: fonts.text,
+    color: colors.heading,
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading,
   },
   button: {
     backgroundColor: colors.green,
@@ -60,8 +91,8 @@ const styles = StyleSheet.create({
     height: 56,
     width: 56,
   },
-  buttonText: {
+  buttonIcon: {
+    fontSize: 32,
     color: colors.white,
-    fontSize: 24,
   }
 });
